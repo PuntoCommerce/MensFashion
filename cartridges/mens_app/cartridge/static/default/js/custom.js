ea4e0e267d9aeacd7d205ed958ebe9f04fb0cac0
Alejandro Monge <alejandro@puntocommerce.com>
2022-04-11 13:01:20 -0500

    footer

--- cartridges/mens_app/cartridge/static/default/js/custom.js
+++ cartridges/mens_app/cartridge/static/default/js/custom.js
@@ -1,14 +1,17 @@
 const $C = (el) => document.querySelector(el);
 const $$C = (el) => document.querySelectorAll(el);
 
-const searchCustom = $C(".search-field");
+const searchFieldDesk = $C(".search-field");
 const searchButton = $C("#searcHeader");
+const searchFieldResponsive = $C(".search-field-responsive");
+const searchButtonResponsive = $C("#searcHeaderResponsive");
 
 //Open the search field when the search button is clicked. Then the search button is used to submit the search.
 searchButton.addEventListener("click", () => {
   if (searchButton.type == "button") {
-    searchCustom.classList.remove("hidden");
-    searchCustom.focus();
+    searchFieldDesk.classList.remove("hidden");
+    searchFieldDesk.focus();
+
     setTimeout(() => {
       //Wait to make the change and not submit the search before the animation is done.
       searchButton.type = "submit";
@@ -17,9 +20,43 @@ searchButton.addEventListener("click", () => {
 });
 
 //Close the search field when the user clicks out of the input and there is not a search words written.
-searchCustom.addEventListener("focusout", () => {
-  if (searchCustom.value == "") {
-    searchCustom.classList.add("hidden");
+searchFieldDesk.addEventListener("focusout", () => {
+  if (searchFieldDesk.value == "") {
+    searchFieldDesk.classList.add("hidden");
     searchButton.type = "button";
+  }
+});
+
+//Open the search field when the search button is clicked. Then the search button is used to submit the search.
+searchButtonResponsive.addEventListener("click", () => {
+  if (searchButtonResponsive.type == "button") {
+    searchFieldResponsive.classList.remove("hidden");
+    searchFieldResponsive.focus();
+
+    setTimeout(() => {
+      //Wait to make the change and not submit the search before the animation is done.
+      searchButtonResponsive.type = "submit";
+    }, 500);
+  }
+});
+
+//Close the search field when the user clicks out of the input and there is not a search words written.
+searchFieldResponsive.addEventListener("focusout", () => {
+  if (searchFieldResponsive.value == "") {
+    searchFieldResponsive.classList.add("hidden");
+    searchButtonResponsive.type = "button";
+  }
+});
+
+/* ########################################## ⬆️ Btn Back to top & Sticky Nav Bar⬆️ ####################################################### */
+// Listen on scroll event to show/hide btn back to top & add sticky nav
+var backToTopBtn = $C(".goToUp");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 700) {
+    backToTopBtn.classList.remove("hidden");
+  } else {
+    backToTopBtn.classList.add("hidden");
   }
 });
