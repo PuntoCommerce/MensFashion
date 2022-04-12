62aaf6b758aed9643c7c2983250df0f9f8998a2a
Alejandro Monge <alejandro@puntocommerce.com>
2022-04-12 15:45:32 -0500

    Search and suggestions, desk and mobile

--- cartridges/mens_app/cartridge/static/default/js/custom.js
+++ cartridges/mens_app/cartridge/static/default/js/custom.js
@@ -1,28 +1,35 @@
 const $C = (el) => document.querySelector(el);
 const $$C = (el) => document.querySelectorAll(el);
 
-const searchFieldDesk = $C(".search-field");
+const searchFieldDesk = $C("#searchFieldDesk");
 const searchButton = $C("#searcHeader");
+// const searchClose = $C(".search-desk-reset-button");
+
 const searchFieldResponsive = $C(".search-field-responsive");
 const searchButtonResponsive = $C("#searcHeaderResponsive");
+const searchContainer = $C("#searchContainer");
+
+const header = $C("#headerContainer");
 
 //Open the search field when the search button is clicked. Then the search button is used to submit the search.
 searchButton.addEventListener("click", () => {
   if (searchButton.type == "button") {
     searchFieldDesk.classList.remove("hidden");
+    // searchClose.classList.remove("hidden");
     searchFieldDesk.focus();
 
     setTimeout(() => {
       //Wait to make the change and not submit the search before the animation is done.
       searchButton.type = "submit";
-    }, 500);
+    }, 100);
   }
 });
 
 //Close the search field when the user clicks out of the input and there is not a search words written.
 searchFieldDesk.addEventListener("focusout", () => {
-  if (searchFieldDesk.value == "") {
+  if (!searchFieldDesk.value) {
     searchFieldDesk.classList.add("hidden");
+    // searchClose.classList.add("hidden");
     searchButton.type = "button";
   }
 });
@@ -30,31 +37,33 @@ searchFieldDesk.addEventListener("focusout", () => {
 //Open the search field when the search button is clicked. Then the search button is used to submit the search.
 searchButtonResponsive.addEventListener("click", () => {
   if (searchButtonResponsive.type == "button") {
-    searchFieldResponsive.classList.remove("hidden");
+    searchContainer.classList.remove("hidden");
+    header.classList.add("hidden");
+
     searchFieldResponsive.focus();
 
     setTimeout(() => {
       //Wait to make the change and not submit the search before the animation is done.
       searchButtonResponsive.type = "submit";
-    }, 500);
+    }, 100);
   }
 });
 
 //Close the search field when the user clicks out of the input and there is not a search words written.
 searchFieldResponsive.addEventListener("focusout", () => {
-  if (searchFieldResponsive.value == "") {
-    searchFieldResponsive.classList.add("hidden");
+  if (!searchFieldResponsive.value) {
+    searchContainer.classList.add("hidden");
+    header.classList.remove("hidden");
+
     searchButtonResponsive.type = "button";
   }
 });
 
 /* ########################################## ⬆️ Btn Back to top & Sticky Nav Bar⬆️ ####################################################### */
 // Listen on scroll event to show/hide btn back to top & add sticky nav
-var backToTopBtn = $C(".goToUp");
+const backToTopBtn = $C(".goToUp");
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(window.scrollY);
   if (window.scrollY > 500) {
     backToTopBtn.classList.remove("hidden");
   } else {
