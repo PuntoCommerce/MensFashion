96cd4be8bf03b5ed3143edb4474ed20a9ead08ca
Alejandro Monge <alejandro@puntocommerce.com>
2022-04-22 11:35:57 -0500

    Category Card - PD component

---
+++ cartridges/mens_app/cartridge/experience/components/commerce_assets/categoryCard.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var Template = require("dw/util/Template");
+var HashMap = require("dw/util/HashMap");
+
+var URLUtils = require("dw/web/URLUtils");
+var ImageTransformation = require("*/cartridge/experience/utilities/ImageTransformation.js");
+
+/**
+ * Render logic for storefront.topBar component.
+ * @param {dw.experience.ComponentScriptContext} context The component script context object.
+ * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commcerce Cloud Plattform.
+ *
+ * @returns {string} The markup to be displayed
+ */
+module.exports.render = function (context, modelIn) {
+  var model = modelIn || new HashMap();
+  var content = context.content;
+
+  model.image = ImageTransformation.getScaledImage(content.image);
+  model.text = content.text;
+  model.category = URLUtils.url(
+    "Search-Show",
+    "cgid",
+    content.category.getID()
+  ).toString();
+
+  model.verticalPosition = content.verticalPosition;
+  model.horizontalPosition = content.horizontalPosition;
+  model.justification = content.justification;
+
+  return new Template(
+    "experience/components/commerce_assets/categoryCard"
+  ).render(model).text;
+};
