cbb3d2cf1bc7401f1f49e19be6bad643af4985e2
LuisHans <luis@puntocommerce.com>
2022-06-21 11:18:20 -0500

    Toggle refinement bar

--- cartridges/mens_app/cartridge/static/default/js/Plp-Custom.js
+++ cartridges/mens_app/cartridge/static/default/js/Plp-Custom.js
@@ -1,24 +1,6 @@
-
-
 /** PLP */
-
-const filterToggle = (toggle) => {
-    if (toggle === true) {
-        refinementBar.classList.add('refinement-bar--show');
-    } else {
-        refinementBar.classList.remove('refinement-bar--show');
-    }
-}
-
-
-const filters = $C("#filter");
 const refinementBar = $C('#refinement-bar');
-const closeRefinementBar = $C('#close-refinement-bar');
-filters.onclick = () => {
-    let toggle = true;
-    filterToggle(toggle);
-}
-closeRefinementBar.onclick = () => {
-    let toggle = false;
-    filterToggle(toggle);
+
+function toggleFilters() {
+    refinementBar.classList.toggle('refinement-bar--show');
 }
