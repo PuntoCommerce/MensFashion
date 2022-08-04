3b3330f70c880365cea1861dd2e02c37890f961b
Alejandro Monge <alejandro@puntocommerce.com>
2022-08-04 12:54:17 -0500

    in store pick up with google maps autocomplete

--- cartridges/mens_app/cartridge/static/default/js/checkoutCustom.js
+++ cartridges/mens_app/cartridge/static/default/js/checkoutCustom.js
@@ -1,1 +1,58 @@
+function setStores(lat, lng, search, data) {
+  const url = search.getAttribute("data-url");
 
+  fetch(`${url}${lat && lng ? `&lat=${lat}&long=${lng}` : ""}`)
+    .then((res) => res.json())
+    .then((json) => (data.innerHTML = json.storesResultsHtml));
+}
+
+function useUserLocation(search, data) {
+  const success = ({ coords }) => {
+    setStores(coords.latitude, coords.longitude, search, data);
+    search.value = coords.latitude + ", " + coords.longitude;
+  };
+
+  const error = () => {
+    alert("Imposible obtener la ubicacion");
+    setStores(undefined, undefined, search, data);
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function initAutocomplete() {
+  const search = document.getElementById("search-store-custom");
+  const clearSearch = document.getElementById("clear-search-store");
+  const userLocationSearch = document.getElementById(
+    "user-location-search-store"
+  );
+
+  const options = {
+    componentRestrictions: { country: "MX" },
+    types: ["geocode"],
+  };
+  const data = document.querySelector(
+    ".store-locator-container div.results.striped"
+  );
+
+  clearSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+    search.value = "";
+    setStores(undefined, undefined, search, data);
+  });
+
+  userLocationSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+    useUserLocation(search, data);
+  });
+
+  const autocomplete = new google.maps.places.Autocomplete(search, options);
+
+  autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+    const lat = place.geometry.location.lat();
+    const lng = place.geometry.location.lng();
+    // currentPositionClient = { lat, lng };
+    setStores(lat, lng, search, data);
+  });
+}
