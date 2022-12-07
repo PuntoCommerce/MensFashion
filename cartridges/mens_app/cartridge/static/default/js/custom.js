cb6c9b80f60f1302e31cf9ca3f44e8ac7fdd1768
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2022-12-07 16:44:50 -0600

    homepage photo tile

--- cartridges/mens_app/cartridge/static/default/js/custom.js
+++ cartridges/mens_app/cartridge/static/default/js/custom.js
@@ -579,7 +579,7 @@ function addToWishlistButton(e, type) {
 window.addEventListener('scroll', () =>{
   const valueScroll = 4*window.scrollY/-12;
   if($('.photo-tile-image')){
-      $('.photo-tile-image').css({"transform": "translate3d(0px,"+ valueScroll+"px, 0px)"});
+      $('#homepage .photo-tile-image').css({"transform": "translate3d(0px,"+ valueScroll+"px, 0px)"});
   }    
 });
 
