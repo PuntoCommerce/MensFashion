aabf1e739aa6d2486ca33c176f3f2e71f3568883
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2022-12-08 19:22:42 -0600

    product helper extend

---
+++ cartridges/mens_app/cartridge/scripts/helpers/productHelpers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var collections = require('*/cartridge/scripts/util/collections');
+var urlHelper = require('*/cartridge/scripts/helpers/urlHelpers');
+
+const base = module.superModule;
+
+/**
+ * Generates a map of string resources for the template
+ *
+ * @returns {ProductDetailPageResourceMap} - String resource map
+ */
+base.getResources = function () {
+    var Resource = require('dw/web/Resource');
+
+    return {
+        // info_selectforstock: Resource.msg('info.selectforstock', 'product',
+        //     'Select Styles for Availability'),
+        assistiveSelectedText: Resource.msg('msg.assistive.selected.text', 'common', null)
+    };
+}
+
+module.exports = base;
