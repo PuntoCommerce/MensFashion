884bdd112dee5e534b7976ec5a6829a7cc7abc7c
DanielE2099 <daniel.estrada@puntocommerce.com>
2022-12-15 12:37:13 -0600

    SendOrderFixed

--- cartridges/mens_app/cartridge/scripts/salescloud/api.js
+++ cartridges/mens_app/cartridge/scripts/salescloud/api.js
@@ -5,18 +5,18 @@ const getToken = () => {
   return token.call().access_token;
 };
 
-const call = (path, method, body) => {
+const call = (path, method, body,token) => {
   return rest.call({
     path: path,
     method: method,
-    token: getToken(),
+    token: token,
     body: body,
   });
 };
 
 module.exports = {
-  sendOrder: (body) =>
-    call("services/apexrest/OpportunityProducts", "POST", body),
+  sendOrder: (body,token) =>
+    call("services/apexrest/OpportunityProducts", "POST", body,token),
   getInventory: () => call("services/apexrest/inventory", "GET", {}),
   getPrices: () => call("services/apexrest/pricebook", "GET", {}),
   sendAccount: (body) => call("services/apexrest/AccountC", "POST", body),
