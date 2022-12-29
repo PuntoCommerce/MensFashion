269bd0d3da97b2f1b3c96f6552713f222bf200e5
Alejandro Monge <alejandro@puntocommerce.com>
2022-12-28 18:45:58 -0600

    🤡

--- cartridges/mens_app/cartridge/scripts/helpers/accountHelpers.js
+++ cartridges/mens_app/cartridge/scripts/helpers/accountHelpers.js
@@ -1,25 +1,42 @@
 const base = module.superModule;
 
+const URLUtils = require('dw/web/URLUtils');
+
+/**
+ * Gets the password reset token of a customer
+ * @param {Object} customer - the customer requesting password reset token
+ * @returns {string} password reset token string
+ */
+function getPasswordResetToken(customer) {
+    const Transaction = require('dw/system/Transaction');
+
+    const passwordResetToken;
+    Transaction.wrap(function () {
+        passwordResetToken = customer.profile.credentials.createResetPasswordToken();
+    });
+    return passwordResetToken;
+}
+
 /**
  * Sends the email with password reset instructions
  * @param {string} email - email for password reset
  * @param {Object} resettingCustomer - the customer requesting password reset
  */
 function sendPasswordResetEmail2(email, resettingCustomer) {
-    var Resource = require('dw/web/Resource');
-    var Site = require('dw/system/Site');
-    var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
+    const Resource = require('dw/web/Resource');
+    const Site = require('dw/system/Site');
+    const emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
 
-    var passwordResetToken = getPasswordResetToken(resettingCustomer);
-    var url = URLUtils.https('Account-SetNewPassword', 'Token', passwordResetToken);
-    var objectForEmail = {
+    const passwordResetToken = getPasswordResetToken(resettingCustomer);
+    const url = URLUtils.https('Account-SetNewPassword', 'Token', passwordResetToken);
+    const objectForEmail = {
         passwordResetToken: passwordResetToken,
         firstName: resettingCustomer.profile.firstName,
         lastName: resettingCustomer.profile.lastName,
         url: url
     };
 
-    var emailObj = {
+    const emailObj = {
         to: email,
         subject: Resource.msg('subject.profile.resetpassword.email', 'login', null),
         from: Site.current.getCustomPreferenceValue('customerServiceEmail') || 'no-reply@testorganization.com',
