cfce0ea9d9521259a1c03c3867ad4e85971863f6
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2022-12-31 08:08:21 -0600

    create Alert to add product

--- cartridges/mens_app/cartridge/static/default/js/accordion.js
+++ cartridges/mens_app/cartridge/static/default/js/accordion.js
@@ -30,3 +30,54 @@ function removeOpen(index1) {
     }
   })
 }
+
+function showAlertItemCart(e) {
+  var button = e.children[0].childNodes[3].disabled;
+  const alertContainer = document.querySelector('.add-item-pdp-alert')
+  const body = document.querySelector('#body-scroll');
+  
+  if (button === true) {
+    $('.add-item-pdp-alert').append(`
+      <div class='add-item-pdp-alert__card'>
+        <span class='add-item-pdp-alert__card__title'>AVISO</span>
+        <span class='add-item-pdp-alert__card__text'>Tienes que seleccionar un color y una talla.</span>
+        <button onclick='hideAlertItem()' class='add-item-pdp-alert__card__button'>CERRAR</buttom>
+      </div>
+    `)
+  
+    alertContainer.style.zIndex = '100'
+    alertContainer.style.background = 'rgba(255, 255, 255, 0.7)'
+    body.classList.add('scroll-none');
+  }
+}
+
+function hideAlertItem() {
+  const alertContainer = document.querySelector('.add-item-pdp-alert')
+  const body = document.querySelector('#body-scroll');
+
+  $('.add-item-pdp-alert .add-item-pdp-alert__card').remove();
+  
+  alertContainer.style.zIndex = '-1'
+  alertContainer.style.background = 'none'
+  body.classList.remove('scroll-none');
+}
+
+function showAlertItemBuy(e) {
+  var button = e.children[0].childNodes[5].disabled;
+  const alertContainer = document.querySelector('.add-item-pdp-alert')
+  const body = document.querySelector('#body-scroll');
+  
+  if (button === true) {
+    $('.add-item-pdp-alert').append(`
+      <div class='add-item-pdp-alert__card'>
+        <span class='add-item-pdp-alert__card__title'>AVISO</span>
+        <span class='add-item-pdp-alert__card__text'>Tienes que seleccionar un color y una talla.</span>
+        <button onclick='hideAlertItem()' class='add-item-pdp-alert__card__button'>CERRAR</buttom>
+      </div>
+    `)
+  
+    alertContainer.style.zIndex = '100'
+    alertContainer.style.background = 'rgba(255, 255, 255, 0.7)'
+    body.classList.add('scroll-none');
+  }
+}
