984882d7d7657e7255a405742274af41418e9124
Myrna90 <myrnamares90@gmail.com>
2023-02-09 12:30:06 -0600

    Commir para bajar cambios de gitHub

--- cartridges/mens_app/cartridge/static/default/js/custom.js
+++ cartridges/mens_app/cartridge/static/default/js/custom.js
@@ -603,12 +603,12 @@ if (widthScreen > 543) {
     active.classList.add('active');
   }
 
-  /* window.onload=function(){
+   window.onload=function(){
     var pos=window.name || 0;
     document.getElementById('plp-products').scrollTop=pos;
     }
     window.onunload=function(){
     window.name=document.getElementById('plp-products').scrollTop;
-    } */
+    }
 
 localValues();
