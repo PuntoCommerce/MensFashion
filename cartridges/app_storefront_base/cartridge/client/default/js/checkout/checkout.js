b5bd569835ffa8e49f26aa9b0443bb7c87de3bed
Myrna90 <myrnamares90@gmail.com>
2023-02-15 21:27:49 -0600

    Edit buttons fixed, back to base functionality

--- cartridges/app_storefront_base/cartridge/client/default/js/checkout/checkout.js
+++ cartridges/app_storefront_base/cartridge/client/default/js/checkout/checkout.js
@@ -442,7 +442,7 @@ var scrollAnimate = require('../components/scrollAnimate');
                     members.gotoStage('customer');
                 });
 
-                /* $('.shipping-summary .edit-button', plugin).on('click', function () {
+                $('.shipping-summary .edit-button', plugin).on('click', function () {
                     if (!$('#checkout-main').hasClass('multi-ship')) {
                         $('body').trigger('shipping:selectSingleShipping');
                     }
@@ -452,9 +452,9 @@ var scrollAnimate = require('../components/scrollAnimate');
 
                 $('.payment-summary .edit-button', plugin).on('click', function () {
                     members.gotoStage('payment');
-                });  */
+                }); 
 
-                $ ('#custom-edit-shipping').on('click', function () {
+               /*  $ ('#custom-edit-shipping').on('click', function () {
                     if (!$('#checkout-main').hasClass('multi-ship')) {
                         $('body').trigger('shipping:selectSingleShipping');
                     }
@@ -464,7 +464,7 @@ var scrollAnimate = require('../components/scrollAnimate');
 
                 $('#custom-edit-payment').on('click', function () {
                     members.gotoStage('payment');
-                }); 
+                });  */
                 //
                 // remember stage (e.g. shipping)
                 //
