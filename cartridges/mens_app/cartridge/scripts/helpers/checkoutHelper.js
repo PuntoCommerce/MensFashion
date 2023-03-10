8f9af34ccf1ff60528aa58af7e48047af41a00aa
Myrna90 <myrnamares90@gmail.com>
2023-03-10 09:39:10 -0600

    PDP design changes added

---
+++ cartridges/mens_app/cartridge/scripts/helpers/checkoutHelper.js
@@ -0,0 +1,8 @@
+const Transaction = require("dw/system/Transaction");
+ const CustomerMgr = require("dw/customer/CustomerMgr");
+ var URLUtils = require('dw/web/URLUtils');
+ const BasketMgr = require('dw/order/BasketMgr');
+
+ function addFieldsCheckout(){
+    
+ }
