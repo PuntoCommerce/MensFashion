3c58cb0c3f35dcc96c156a229472846c5b7a432e
Myrna90 <myrnamares90@gmail.com>
2023-07-12 17:53:13 -0600

    No funciona el localStorage

--- cartridges/mens_app/cartridge/static/default/js/accordion.js
+++ cartridges/mens_app/cartridge/static/default/js/accordion.js
@@ -1,141 +1,141 @@
-const accordion = document.querySelectorAll('#accordion-content');
-
+const accordion = document.querySelectorAll("#accordion-content");
 
 accordion.forEach((item, index) => {
-  let button = item.querySelector('button');
-  if (button) {
-    button.addEventListener('click', () => {
-      item.classList.toggle('open');
+    let button = item.querySelector("button");
+    if (button) {
+        button.addEventListener("click", () => {
+            item.classList.toggle("open");
 
-      let description = item.querySelector('.accordion-description')
+            let description = item.querySelector(".accordion-description");
 
-      if (item.classList.contains('open')) {
-        description.style.height = `${description.scrollHeight}px`;
-      } else {
-        description.style.height = "0px";
-      }
-      removeOpen(index);
-    })
-  }
-})
+            if (item.classList.contains("open")) {
+                description.style.height = `${description.scrollHeight}px`;
+            } else {
+                description.style.height = "0px";
+            }
+            removeOpen(index);
+        });
+    }
+});
 
 function removeOpen(index1) {
-  accordion.forEach((item2, index2) => {
-    if (index1 != index2) {
-      console.log(item2);
-      item2.classList.remove('open');
+    accordion.forEach((item2, index2) => {
+        if (index1 != index2) {
+            console.log(item2);
+            item2.classList.remove("open");
 
-      let desc = item2.querySelector('.accordion-description');
+            let desc = item2.querySelector(".accordion-description");
 
-      desc.style.height = "0px";
-
-    }
-  })
+            desc.style.height = "0px";
+        }
+    });
 }
 
 function showAlertItemCart(e) {
-  var button = e.children[0].childNodes[3].disabled;
-  const alertContainer = document.querySelector('.add-item-pdp-alert')
-  const body = document.querySelector('#body-scroll');
-  const colors = document.querySelectorAll('#colorsSelecteable');
-  const selectSize = document.querySelector('#talla-1');
-  var messages = {};
-  var message;
-  var select = selectSize.value;
+    var button = e.children[0].childNodes[3].disabled;
+    const alertContainer = document.querySelector(".add-item-pdp-alert");
+    const body = document.querySelector("#body-scroll");
+    const colors = document.querySelectorAll("#colorsSelecteable");
+    const selectSize = document.querySelector("#talla-1");
+    var messages = {};
+    var message;
+    var select = selectSize.value;
 
-  colors.forEach((item, index) => {
-    const colorSwatch = item.querySelector('span');
-    colorSwatch.classList.contains('selected') ?
-      messages.success = 'seleccionado' :
-      messages.error = 'color';
-  })
+    colors.forEach((item, index) => {
+        const colorSwatch = item.querySelector("span");
+        colorSwatch.classList.contains("selected")
+            ? (messages.success = "seleccionado")
+            : (messages.error = "color");
+    });
 
-  if (select === 'talla' && messages.error) {
-    message = 'Tienes que seleccionar un color y una talla.'
-  } else if (select = 'talla' && messages.success) {
-    message = 'Tienes que seleccionar una talla'
-  } else if (!select && !messages.success) {
-    message = 'Tienes que seleccionar un color'
-  }
+    if (select === "talla" && messages.error) {
+        message = "Tienes que seleccionar un color y una talla.";
+    } else if ((select = "talla" && messages.success)) {
+        message = "Tienes que seleccionar una talla";
+    } else if (!select && !messages.success) {
+        message = "Tienes que seleccionar un color";
+    }
 
-  if (button === true) {
-    $('.add-item-pdp-alert').append(`
+    if (button === true) {
+        $(".add-item-pdp-alert").append(`
       <div class='add-item-pdp-alert__card'>
         <span class='add-item-pdp-alert__card__title'>AVISO</span>
         <span class='add-item-pdp-alert__card__text'>${message}</span>
         <button onclick='hideAlertItem()' class='add-item-pdp-alert__card__button'>CERRAR</buttom>
       </div>
-    `)
+    `);
 
-    alertContainer.style.zIndex = '100'
-    alertContainer.style.background = 'rgba(255, 255, 255, 0.7)'
-    body.classList.add('scroll-none');
-  }
+        alertContainer.style.zIndex = "100";
+        alertContainer.style.background = "rgba(255, 255, 255, 0.7)";
+        body.classList.add("scroll-none");
+    }
 }
 
 function showAlertItemBuy(e) {
-  var button = e.children[0].childNodes[5].disabled;
-  const alertContainer = document.querySelector('.add-item-pdp-alert')
-  const body = document.querySelector('#body-scroll');
-  const colors = document.querySelectorAll('#colorsSelecteable');
-  const selectSize = document.querySelector('#talla-1');
-  var messages = {};
-  var message;
-  var select = selectSize.value;
+    var button = e.children[0].childNodes[5].disabled;
+    const alertContainer = document.querySelector(".add-item-pdp-alert");
+    const body = document.querySelector("#body-scroll");
+    const colors = document.querySelectorAll("#colorsSelecteable");
+    const selectSize = document.querySelector("#talla-1");
+    var messages = {};
+    var message;
+    var select = selectSize.value;
 
-  colors.forEach((item, index) => {
-    const colorSwatch = item.querySelector('span');
-    colorSwatch.classList.contains('selected') ?
-      messages.success = 'seleccionado' :
-      messages.error = 'color';
-  })
+    colors.forEach((item, index) => {
+        const colorSwatch = item.querySelector("span");
+        colorSwatch.classList.contains("selected")
+            ? (messages.success = "seleccionado")
+            : (messages.error = "color");
+    });
 
-  if (select === 'talla' && messages.error) {
-    message = 'Tienes que seleccionar un color y una talla.'
-  } else if (select = 'talla' && messages.success) {
-    message = 'Tienes que seleccionar una talla'
-  } else if (!select && !messages.success) {
-    message = 'Tienes que seleccionar un color'
-  }
+    if (select === "talla" && messages.error) {
+        message = "Tienes que seleccionar un color y una talla.";
+    } else if ((select = "talla" && messages.success)) {
+        message = "Tienes que seleccionar una talla";
+    } else if (!select && !messages.success) {
+        message = "Tienes que seleccionar un color";
+    }
 
-  if (button === true) {
-    $('.add-item-pdp-alert').append(`
+    if (button === true) {
+        $(".add-item-pdp-alert").append(`
       <div class='add-item-pdp-alert__card'>
         <span class='add-item-pdp-alert__card__title'>AVISO</span>
         <span class='add-item-pdp-alert__card__text'>${message}</span>
         <button onclick='hideAlertItem()' class='add-item-pdp-alert__card__button'>CERRAR</buttom>
       </div>
-    `)
+    `);
 
-    alertContainer.style.zIndex = '100'
-    alertContainer.style.background = 'rgba(255, 255, 255, 0.7)'
-    body.classList.add('scroll-none');
-  }
+        alertContainer.style.zIndex = "100";
+        alertContainer.style.background = "rgba(255, 255, 255, 0.7)";
+        body.classList.add("scroll-none");
+    }
 }
 
 function hideAlertItem() {
-  const alertContainer = document.querySelector('.add-item-pdp-alert')
-  const body = document.querySelector('#body-scroll');
+    const alertContainer = document.querySelector(".add-item-pdp-alert");
+    const body = document.querySelector("#body-scroll");
 
-  $('.add-item-pdp-alert .add-item-pdp-alert__card').remove();
+    $(".add-item-pdp-alert .add-item-pdp-alert__card").remove();
 
-  alertContainer.style.zIndex = '-1'
-  alertContainer.style.background = 'none'
-  body.classList.remove('scroll-none');
+    alertContainer.style.zIndex = "-1";
+    alertContainer.style.background = "none";
+    body.classList.remove("scroll-none");
 }
 
-const paymentOptions = document.querySelectorAll('.payment-options .nav-item');
+const paymentOptions = document.querySelectorAll(".payment-options .nav-item");
 
 paymentOptions.forEach((item, index) => {
-  const buttonPayment = document.querySelector('.submit-payment');
+    const buttonPayment = document.querySelector(".submit-payment");
 
-  item.addEventListener('click', (e) => {
-
-    setTimeout(() => {
-      e.path[0].classList.contains('active') && e.path[0].classList.contains('openpay-tab') ? buttonPayment.classList.remove('d-none') : '';
-    }, 200);
-  })
-})
+    item.addEventListener("click", (e) => {
+        setTimeout(() => {
+            e.path[0].classList.contains("active") &&
+            e.path[0].classList.contains("openpay-tab")
+                ? buttonPayment.classList.remove("d-none")
+                : "";
+        }, 200);
+    });
+});
 
 // function nextStep(e) {
 //   const buttonShipping = e.children[0].children[1].disabled;
@@ -145,72 +145,86 @@ paymentOptions.forEach((item, index) => {
 // }
 
 function homeRedirectLogin() {
-  url = document.getElementById('redirectlogin').dataset.href;
-  sessionStorage.setItem('homeAlertRegister', true)
+    url = document.getElementById("redirectlogin").dataset.href;
+    sessionStorage.setItem("homeAlertRegister", true);
 
-  window.location.replace(url)
+    window.location.replace(url);
 }
 
 function checkoutRedirectLogin() {
-  sessionStorage.setItem('checkoutAlertRegister', true)
-  document.getElementById('checkout-alert').classList.add('d-none')
+    sessionStorage.setItem("checkoutAlertRegister", true);
+    document.getElementById("checkout-alert").classList.add("d-none");
 }
 
 function closeAlertPromotion() {
-  const checkout = document.getElementById('checkout-alert');
-  const home = document.getElementById('homepage-alert');
-  const popupCheckout = document.getElementById('modal-check');
+    const checkout = document.getElementById("checkout-alert");
+    const home = document.getElementById("homepage-alert");
+    const popupCheckout = document.getElementById("modal-check");
+    const isClose = localStorage.getItem("modal-check");
 
-  if (checkout) {
-    checkout.classList.add('d-none')
-    sessionStorage.setItem('checkoutAlertRegister', true)
-  };
-  if (home) {
-    home.classList.add('d-none')
-    sessionStorage.setItem('homeAlertRegister', true)
-  };
+    if (checkout) {
+        checkout.classList.add("d-none");
+        sessionStorage.setItem("checkoutAlertRegister", true);
+    }
+    if (home) {
+        home.classList.add("d-none");
+        sessionStorage.setItem("homeAlertRegister", true);
+    }
+
+    if (popupCheckout || isClose === false) {
+        popupCheckout.classList.add("d-none");
+        sessionStorage.setItem("checkoutPopupAlert", true);
+    }
 }
 
-function closeAlertModal(){
-  const popupCheckout = document.getElementById('modal-check');
-  popupCheckout.classList.add('d-none');
-}
-
-if (sessionStorage.getItem('homeAlertRegister') === null && document.getElementById('homepage-alert')) document.getElementById('homepage-alert').classList.remove('d-none');
-if (sessionStorage.getItem('checkoutAlertRegister') === null && document.getElementById('checkout-alert')) document.getElementById('checkout-alert').classList.remove('d-none');
+if (
+    sessionStorage.getItem("homeAlertRegister") === null &&
+    document.getElementById("homepage-alert")
+)
+    document.getElementById("homepage-alert").classList.remove("d-none");
+if (
+    sessionStorage.getItem("checkoutAlertRegister") === null &&
+    document.getElementById("checkout-alert")
+)
+    document.getElementById("checkout-alert").classList.remove("d-none");
+if (
+    sessionStorage.getItem("checkoutPopupAlert") === null &&
+    document.getElementById("modal-check")
+)
+    document.getElementById("modal-check").classList.remove("d-none");
 
 function categoryBreadcums(e) {
-  setTimeout(() => {
-    const ul = document.querySelectorAll('.values.content.breadcums');
-    ul.forEach(item => {
-      var itemSelected = item.querySelectorAll('li button span');
+    setTimeout(() => {
+        const ul = document.querySelectorAll(".values.content.breadcums");
+        ul.forEach((item) => {
+            var itemSelected = item.querySelectorAll("li button span");
 
-      itemSelected.forEach((select) => {
-
-        select.classList.contains('selected') ? $('.plp-tools__item--breadcrums').append(`
+            itemSelected.forEach((select) => {
+                select.classList.contains("selected")
+                    ? $(".plp-tools__item--breadcrums").append(`
          <li>
           <a class='breadcums-subcategories'>${select.innerHTML}</a>
          </li>
         `)
-          : '';
-      })
-    })
-  }, 1000);
+                    : "";
+            });
+        });
+    }, 1000);
 }
 
-const productId = document.querySelector('.productID .productID__id');
+const productId = document.querySelector(".productID .productID__id");
 
 if (productId) {
-  window.addEventListener('change', () => {
-    setTimeout(() => {
-      const productId = document.querySelector('.productID .productID__id');
-      const SKU = document.querySelector('.productSku .product-id');
+    window.addEventListener("change", () => {
+        setTimeout(() => {
+            const productId = document.querySelector(
+                ".productID .productID__id"
+            );
+            const SKU = document.querySelector(".productSku .product-id");
 
-      if (productId.innerText != SKU.innerText) {
-        $('.productSku').removeClass('d-none')
-      };
-    }, 500);
-  })
+            if (productId.innerText != SKU.innerText) {
+                $(".productSku").removeClass("d-none");
+            }
+        }, 500);
+    });
 }
-
-
